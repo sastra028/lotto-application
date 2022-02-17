5706ccd6beb73a41e0fe2e4573079d2a6fa56ec6
sastra tiparos <sastra.028@gmail.com>
2022-02-17 11:05:02 +0700

    move front

---
+++ src/main/front/src/environments/environment.production.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  sessionMinutes: 20 * 60 * 1000,
+  SECURITY: {
+    STORAGE_JWT_KEY: "CURRENT_JWT"
+  }
+};
+// api url
+export const API_URL = "http://localhost:8081";
