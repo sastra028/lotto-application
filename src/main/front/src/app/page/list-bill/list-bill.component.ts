0a2cf9b1ef3d0ed17b3765bd94a465a1263870fd
sastra tiparos <sastra.028@gmail.com>
2022-02-19 14:55:20 +0700

    new laout

--- src/main/front/src/app/page/list-bill/list-bill.component.ts
+++ src/main/front/src/app/page/list-bill/list-bill.component.ts
@@ -13,7 +13,7 @@ export class ListBillComponent implements OnInit {
 
   loadingspin = false;
   islangThai?:boolean;
-
+  
   
   sessionHistory:any = [
     {coursecode: 1, name: 'A', classnameeng: 1.0079, acadyear: '2565', classdate: '19/02/2022', classtime: '10:29'},
@@ -33,9 +33,164 @@ export class ListBillComponent implements OnInit {
 
   ];
 
+    
+  billLists:any = [
+    {billId: 1, name: 'A'},
+    {billId: 2, name: 'B'},
+
+  ];
+
+  billList:any = [
+    {
+      billId: 1, 
+      name: 'ป้านุ', 
+      update: '17/02/2022-12:33', 
+
+      listTwoUpTotal: "200",
+      listTwoUpPrize: "100",
+
+      listTwoDownTotal: "200",
+      listTwoDownPrize: "2000",
+
+      listThreeDirectTotal: "200",
+      listThreeDirectPrize: "200",
+
+      listThreeToteTotal: "200",
+      listThreeTotePrize: "200",
+
+      listTotal: "20000",
+      prizeTotal: "10000",
+      listTwoUp:[
+        {
+          number:"02",
+          price:"20",
+          prize: false
+        },
+        {
+          number:"03",
+          price:"20",
+          prize: true
+        }
+      ],
+      listTwoDown:[
+        {
+          number:"02",
+          price:"20",
+          prize: false
+        },
+        {
+          number:"03",
+          price:"20",
+          prize: true
+        }
+      ],
+      listThreeDirect:[
+        {
+          number:"159",
+          price:"100",
+          prize: false
+        },
+        {
+          number:"657",
+          price:"100",
+          prize: false
+        }
+      ],
+      listThreeTote:[
+        {
+          number:"935",
+          price:"50",
+          prize: false
+        },
+        {
+          number:"864",
+          price:"50",
+          prize: false
+        }
+      ],
+  
+    },
+    
+    {
+      billId: 2, 
+      name: 'ป้านะ', 
+      update: '17/02/2022-12:33', 
+
+      listTwoUpTotal: "200",
+      listTwoUpPrize: "100",
+
+      listTwoDownTotal: "200",
+      listTwoDownPrize: "2000",
+
+      listThreeDirectTotal: "200",
+      listThreeDirectPrize: "200",
+
+      listThreeToteTotal: "200",
+      listThreeTotePrize: "200",
+
+      listTotal: "20000",
+      prizeTotal: "10000",
+      listTwoUp:[
+        {
+          number:"02",
+          price:"20",
+          prize: false
+        },
+        {
+          number:"03",
+          price:"20",
+          prize: true
+        }
+      ],
+      listTwoDown:[
+        {
+          number:"02",
+          price:"20",
+          prize: false
+        },
+        {
+          number:"03",
+          price:"20",
+          prize: true
+        }
+      ],
+      listThreeDirect:[
+        {
+          number:"159",
+          price:"100",
+          prize: false
+        },
+        {
+          number:"657",
+          price:"100",
+          prize: false
+        }
+      ],
+      listThreeTote:[
+        {
+          number:"935",
+          price:"50",
+          prize: false
+        },
+        {
+          number:"864",
+          price:"50",
+          prize: false
+        }
+      ],
+  
+  
+    }
+
+  ];
+
   classnamecurrent:any;
   translatS: any;
   
+  formSearch:any = {};
+  classes:any = [];
+
+
   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
   from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
   originally bred for hunting.`;
@@ -58,6 +213,26 @@ export class ListBillComponent implements OnInit {
   }
 
   async openSession(session:any){
+
+  }
+
+  
+  async onScrollDownFreesearch(ev: any) {
+
+  }
+
+  async filterFreesearch(){
+
+  }
+  select(item:any) {
+
+  }
+
+  getCourseName(item:any) {
+
+  }
+
+  toDisplayYear(year:any) {
 
   }
 }
